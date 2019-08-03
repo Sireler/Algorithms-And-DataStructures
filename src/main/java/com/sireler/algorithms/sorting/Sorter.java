e18a9f0c6db4eb3a9e70cd172847f5c9e0b986bf
Sireler <sireler@gmail.com>
2019-08-03 19:55:40 +0300

    Bubble sort and tests

--- src/main/java/com/sireler/algorithms/sorting/Sorter.java
+++ src/main/java/com/sireler/algorithms/sorting/Sorter.java
@@ -1,7 +1,19 @@
 package com.sireler.algorithms.sorting;
 
-public class Sorter {
-    public int[] bubble(int[] array) {
-        return array;
+class Sorter {
+    int[] bubble(int[] array) {
+        int[] arr = array.clone();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        return arr;
     }
 }
--- src/test/java/com/sireler/algorithms/sorting/SorterTest.java
+++ src/test/java/com/sireler/algorithms/sorting/SorterTest.java
@@ -1,12 +1,24 @@
 package com.sireler.algorithms.sorting;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SorterTest {
 
+    private static final int[] ARRAY = { 5, 4, 1, 3, 2, 7, 9, 6, 8 };
+    private static final int[] EXPECTED = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    private Sorter sorter;
+
+    @Before
+    public void beforeTest() {
+        this.sorter = new Sorter();
+    }
+
     @Test
     public void bubble() {
+        assertArrayEquals(EXPECTED, sorter.bubble(ARRAY));
     }
 }
