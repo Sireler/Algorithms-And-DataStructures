e1dceffb272bba969e93368c2b9128627ded1f6c
Sireler <sireler@gmail.com>
2019-08-03 19:40:11 +0300

    Sorter class and tests

---
+++ src/main/java/com/sireler/algorithms/sorting/Sorter.java
@@ -0,0 +1,7 @@
+package com.sireler.algorithms.sorting;
+
+public class Sorter {
+    public int[] bubble(int[] array) {
+        return array;
+    }
+}
---
+++ src/test/java/com/sireler/algorithms/sorting/SorterTest.java
@@ -0,0 +1,12 @@
+package com.sireler.algorithms.sorting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SorterTest {
+
+    @Test
+    public void bubble() {
+    }
+}
