2ccb383ad78f44992a882a1e86f3957eac5e12ae
Sireler <sireler@gmail.com>
2019-08-08 15:45:19 +0300

    Private constructor for Sorter

--- src/main/java/com/sireler/algorithms/sorting/Sorter.java
+++ src/main/java/com/sireler/algorithms/sorting/Sorter.java
@@ -2,6 +2,9 @@ package com.sireler.algorithms.sorting;
 
 public class Sorter {
 
+    private Sorter() {
+    }
+
     public static int[] bubble(int[] array) {
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length - 1 - i; j++) {
